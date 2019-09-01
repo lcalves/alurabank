7c0bca3388a5c030a0d468a41e8f9b3929f0a4cb
Leonardo Campos <leonardo_lca@yahoo.com.br>
2019-09-01 20:13:37 -0300

    adicionando JQuery para compatibilidade com smartphones antigos.

--- app/ts/app.ts
+++ app/ts/app.ts
@@ -1,3 +1,4 @@
 const controller = new NegociacaoController();
 
-document.querySelector('.form').addEventListener('submit', controller.adiciona.bind(controller))
+//Utilizando JQquery para compatibilidade com aplicativos android antigos
+$('.form').submit(controller.adiciona.bind(controller));
--- app/ts/views/View.ts
+++ app/ts/views/View.ts
@@ -1,13 +1,13 @@
 abstract class View<T>{
 
-    protected _elemento: Element;
+    protected _elemento: JQuery;
 
     constructor(seletor: string){
-        this._elemento = document.querySelector(seletor)
+        this._elemento = $(seletor)
     }
 
     update(modelo: T): void{
-        this._elemento.innerHTML =  this.template(modelo);
+        this._elemento.html(this.template(modelo));
 
     }
 
