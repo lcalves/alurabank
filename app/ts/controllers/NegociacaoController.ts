ba11a8a4b6fa85a21deadd409d98c7b9207694cf
Leonardo Campos <leonardo_lca@yahoo.com.br>
2019-09-01 18:31:06 -0300

    adicionado template string para dinamizar a tabela de negociacoes

--- app/ts/controllers/NegociacaoController.ts
+++ app/ts/controllers/NegociacaoController.ts
@@ -1,22 +1,28 @@
 class NegociacaoController{
 
-    private _inputData;
-    private _inputQuantidade;
-    private _inputValor;
+    private _inputData: HTMLInputElement ;
+    private _inputQuantidade: HTMLInputElement;
+    private _inputValor: HTMLInputElement;
+    private _negociacoes = new Negociacoes();
+    private _negociacoesView = new NegociacoesView('#negociacoesView');
+
 
     constructor(){
-        this._inputData = document.querySelector('#data');
-        this._inputQuantidade = document.querySelector('#quantidade');
-        this._inputValor = document.querySelector('#valor');
+        this._inputData = <HTMLInputElement>document.querySelector('#data');
+        this._inputQuantidade =<HTMLInputElement>document.querySelector('#quantidade');
+        this._inputValor = <HTMLInputElement>document.querySelector('#valor');
+        this._negociacoesView.update(this._negociacoes);
     }
-
-    adiciona(event){
+    
+    adiciona(event: Event){
         event.preventDefault();
         const negociacao = new Negociacao(
-            this._inputData, 
-            this._inputQuantidade,
-            this._inputValor
+            new Date(this._inputData.value.replace(/-/g, ',')), 
+            parseInt(this._inputQuantidade.value),
+            parseFloat(this._inputValor.value)
             );
-            console.log('Logica aqui', negociacao);
+            this._negociacoes.adiciona(negociacao);
+            this._negociacoesView.update(this._negociacoes);
+            
     }
 }
